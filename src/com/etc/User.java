6cb997a11ce8ab09be0f5417468a8e7aa5d5edb6
puhongwei <260812014@qq.com>
2019-09-29 10:11:36 +0800

    sdfdsfds

--- src/com/etc/User.java
+++ src/com/etc/User.java
@@ -5,6 +5,9 @@ public class User {
 	private  String name;
 	private int age;
 	
+	private String address;
+	
+	
 	private String password;
 	
 	public String getName() {
@@ -19,4 +22,21 @@ public class User {
 	public void setAge(int age) {
 		this.age = age;
 	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 }
+
+
